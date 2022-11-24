e41faf53d4e304b8f60c135b88ad962fd7bd6ac9
Alain <ark2784@gmail.com>
2022-11-24 10:19:24 -0300

    rent payment

--- src/components/pages/Rent.js
+++ src/components/pages/Rent.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Rent() {
     return (
@@ -18,11 +19,15 @@ function Rent() {
           
               <br/>
               <h2>
-            2 HOURS 200 USD 
+            2 HOURS 100 USD 
+            <a target="_blanck" className="greenButton" href="https://checkout.stripe.com/c/pay/cs_live_a1iE5oRmN8eFl9iltXW7xAlUiMXzwy0YgKn0QYCOMmCmyuNjTVfy9HCUmY#fidkdWxOYHwnPyd1blppbHNgWjA0Tnc3NkRDclV1VWtVQH1DR3Z%2FUkliNGBidUdBUU1ub0JOXFJXU0lhPEpcQ3Rkd1wyQ3V0QW1KU3ZdNWJxS1N%2FNnBhRFdDblBnVzY9NW1WY0NjYX1yTDBkNTVMcW9wfUQwMScpJ3VpbGtuQH11anZgYUxhJz8nZEBQPEF3ZGBkMTVnMV0zNDEzJ3gl" >Rent Now</a>
             </h2>
+            
             <br/>
             <h2>
-            4 HOURS 350 USD
+            4-5 HOURS 170 USD
+            <a  target="_blanck" className="greenButton" href="https://checkout.stripe.com/c/pay/cs_live_a1iE5oRmN8eFl9iltXW7xAlUiMXzwy0YgKn0QYCOMmCmyuNjTVfy9HCUmY#fidkdWxOYHwnPyd1blppbHNgWjA0Tnc3NkRDclV1VWtVQH1DR3Z%2FUkliNGBidUdBUU1ub0JOXFJXU0lhPEpcQ3Rkd1wyQ3V0QW1KU3ZdNWJxS1N%2FNnBhRFdDblBnVzY9NW1WY0NjYX1yTDBkNTVMcW9wfUQwMScpJ3VpbGtuQH11anZgYUxhJz8nZEBQPEF3ZGBkMTVnMV0zNDEzJ3gl" >Rent Now</a>
+            
             </h2>
             <h2>
               {/* Camila */}
